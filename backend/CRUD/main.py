5da8a67dfd783832529f6b2d72fd8376bf68d88f
JJhano <jhanobk@gmail.com>
2024-05-09 01:34:55 -0400

    corrected

--- backend/CRUD/main.py
+++ backend/CRUD/main.py
@@ -2,14 +2,12 @@ import uvicorn
 import logging
 from app.core.Settings import settings
 import sys
-from app.routes.ubication import get_ubications 
 # def setup_logging():
 #     logging.basicConfig(level=logging.ERROR, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 def run_app():
-    uvicorn.run(app="app.app:app", port=settings.PORT, host=settings.HOST, loop='asyncio', log_level='debug', reload=True, stream=sys.stdout)
+    uvicorn.run(app="app.app:app", port=settings.PORT, host=settings.HOST, loop='asyncio', log_level='debug', reload=True)
 
 if __name__ == "__main__":
     # setup_logging()  # Configurar el registro antes de ejecutar la aplicación
-    # get_ubications()
     run_app()
