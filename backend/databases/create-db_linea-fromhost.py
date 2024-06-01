2a0c9309310e6ab639f0a8710fab94d58f47b385
Benja303 <anugamer303@gmail.com>
2024-05-31 23:03:24 -0400

    Add static data to Db

--- backend/databases/create-db_linea-fromhost.py
+++ backend/databases/create-db_linea-fromhost.py
@@ -1,33 +1,39 @@
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker, declarative_base
+
 # from core.Settings import settings
 import sys
+
 try:
     sys.path.append(".")
     from api.app.models.models import (
-        Line, 
+        Line,
         Brand,
         Model,
         Sector,
-        Microbus, 
-        )
+        Microbus,
+    )
     from api.app.core.Settings import SQLALCHEMY_DATABASE_URL
 except Exception as e:
     print("Error: Can't import")
-    print("Execute from the root folder of backend like this: \npython databases/create_db_linea-fromhost.py")
+    print(
+        "Execute from the root folder of backend like this: \npython databases/create_db_linea-fromhost.py"
+    )
     print("ERROR: ", e)
     sys.exit(1)
 # from core.conexion_db import engine
 from sqlalchemy import create_engine
 
-DATABASE_URL = str(SQLALCHEMY_DATABASE_URL("postgres", "postgres", "localhost", 5432, "db_linea"))
+DATABASE_URL = str(
+    SQLALCHEMY_DATABASE_URL("postgres", "postgres", "localhost", 5432, "db_linea")
+)
 # engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URL))
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 print(f"Sesion iniciada")
 
 
-def queryID(data,state):
+def queryID(data, state):
     try:
         session = SessionLocal()
         if state == "id":
@@ -38,6 +44,7 @@ def queryID(data,state):
             return session.query(type(data)).filter_by(number=data.number).first()
     finally:
         session.close()
+
 
 def addToDB(data, existing_data):
     try:
@@ -55,22 +62,43 @@ def addToDB(data, existing_data):
         session.close()
         return data
 
+
 CANTIDAD_DATOS = 5
 if __name__ == "__main__":
     try:
         session = SessionLocal()
         print("Inserting data...")
-        linea = Line(number=1, color="Rojo")
-        linea_d = addToDB(linea, queryID(linea, "number"))
-        if linea_d:
-            linea = linea_d
-        brand = Brand(name="Mercedes Benz")
-        brand_d = addToDB(brand, queryID(brand, "id"))
-        if brand_d:
-            brand = brand_d
-        for i in range(CANTIDAD_DATOS):
-            micro = Microbus(patent=f"{i}k", line_id=linea.number, brand_id=brand.id)
-            addToDB(micro, queryID(micro, "patent"))
+        with open("staticData/line.csv", "r") as file:
+            lines = file.readlines()
+            for row in lines[1:]:
+                linea = Line(number=row[0], color=row[1])
+                linea_d = addToDB(linea, queryID(linea, "number"))
+                if linea_d:
+                    linea = linea_d
+
+        # linea = Line(number=1, color="Rojo")
+        # linea_d = addToDB(linea, queryID(linea, "number"))
+        # if linea_d:
+        #     linea = linea_d
+        with open("staticData/line.csv", "r") as file:
+            brands = file.readlines()
+            for row in brands[1:]:
+                brand = Brand(name=row[0])
+                brand_d = addToDB(brand, queryID(brand, "id"))
+                if brand_d:
+                    brand = brand_d
+        # brand = Brand(name="Mercedes Benz")
+        # brand_d = addToDB(brand, queryID(brand, "id"))
+        # if brand_d:
+        #     brand = brand_d
+        with open("staticData/line.csv", "r") as file:
+            micros = file.readlines()
+            for row in micros[1:]:
+                micro = Microbus(patent=row[0], line_id=row[1], brand_id=row[2])
+                addToDB(micro, queryID(micro, "patent"))
+        # for i in range(CANTIDAD_DATOS):
+        #     micro = Microbus(patent=f"{i}k", line_id=linea.number, brand_id=brand.id)
+        #     addToDB(micro, queryID(micro, "patent"))
         micro = session.query(Microbus).all()
         print(micro)
     finally:
